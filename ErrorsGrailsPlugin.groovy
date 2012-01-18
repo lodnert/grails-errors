116a185e8049ecdf3bb9bdc3d724e30cd38d62fc
Craig Raw <craig@quirk.biz>
2012-01-18 16:33:17 +0200

    Bump to ver 0.5

--- ErrorsGrailsPlugin.groovy
+++ ErrorsGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class ErrorsGrailsPlugin {
     // the plugin version
-    def version = "0.4"
+    def version = "0.5"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
