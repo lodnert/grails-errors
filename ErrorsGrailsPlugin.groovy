ef79fe32fb00ed77052dbd39f5e74556db71d51c
Craig Raw <craig@quirk.biz>
2012-05-30 13:58:54 +0200

    ver 0.7 for grails plugin portal

--- ErrorsGrailsPlugin.groovy
+++ ErrorsGrailsPlugin.groovy
@@ -1,32 +1,21 @@
 class ErrorsGrailsPlugin {
-    // the plugin version
-    def version = "0.6"
-    // the version or versions of Grails the plugin is designed for
+    def version = "0.7"
     def grailsVersion = "2.0 > *"
-    // the other plugins this plugin depends on
     def dependsOn = [:]
-    // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
     ]
-
-    // TODO Fill in these fields
-    def title = "Errors Plugin" // Headline display name of the plugin
+    def title = "Errors Plugin"
     def author = "Craig Raw"
     def authorEmail = "craig@quirk.biz"
     def description = '''\
-This plugin sets up some error handling for your application. 
-It also looks for GSP pages matching the Grails convention of finding resources before rendering the 404 page, allowing pages to be added without further configuration.
+This plugin sets up some basic error handling for your application. Specifically, it creates a default ErrorController for handling 403, 404 and 500 status codes, along with accompanying GSPs.
+
+It also looks for GSP pages (matching the Grails convention of finding resources) before rendering the 404 page, allowing pages to be added without creating controllers or further configuration of the URL mappings. 
+This is useful in allowing GSP pages to be added to your project by people not familiar with Groovy/Grails.
 '''
-
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/errors"
-
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
     def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
     def organization = [ name: "Quirk Labs", url: "http://www.quirklabs.co.za" ]
+    def documentation = "https://github.com/quirklabs/grails-errors"
+    def scm = [ url: "https://github.com/quirklabs/grails-errors" ]
 }
